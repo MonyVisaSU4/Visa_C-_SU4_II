a6369fdf99e49d91066c07370c14358fba0f85e3
VisaZinSU4 <visagoodboy042@gmail.com>
2023-12-27 09:38:37 +0700

    Project New

---
+++ Lesson2Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Lesson2Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 1;
+            string name = "Ko Tang eii na mo";
+            string gender = "M";
+            string address = "PP";
+            int tell = 098424837;
+            Console.WriteLine("-----Report_User-----");
+            Console.WriteLine("ID:" + id);
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Gender:" + gender);
+            Console.WriteLine("Address:" + address);
+            Console.WriteLine("Please Tell Me:" + tell);
+            Console.WriteLine("---------------------");
+          
+        }
+    }
+}
+
---
+++ Lesson2Lab2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name;
+            string gender;
+            string address;
+            int tell;
+            Console.WriteLine("------Input_data-------");
+            Console.Write("ID:");
+            id=int.Parse(Console.ReadLine());
+            Console.Write("Name:");
+            name=Console.ReadLine();
+            Console.Write("Gender:");
+            gender=Console.ReadLine();
+            Console.Write("Address:");
+            address=Console.ReadLine();
+            Console.Write("Please Tell:");
+            tell = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------Report_User------------------------");
+            Console.WriteLine("ID:" +id);
+            Console.WriteLine("Name:" +name);
+            Console.WriteLine("Gender:" +gender);
+            Console.WriteLine("Address:" +address);
+            Console.WriteLine("Please Tell:" +tell);
+            Console.WriteLine("-----------------------------------------------------------");
+            
+            
+        }
+    }
+}
